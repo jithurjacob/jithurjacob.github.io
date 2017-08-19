59f6b1dc4d8ac8fb9f1bc9f620aa42c1fde16fed
Jithu R Jacob <jithurjacob@gmail.com>
2017-08-19 13:38:36 +0530

    few changes

--- js/text-autotype.js
+++ js/text-autotype.js
@@ -1,6 +1,6 @@
 var i = 1;
 var j= -1;
-var list = ['Pythonista | Automation Consultant @Cognizant'];
+var list = ['Pythonista | Machine Learning Engineer @Cognizant'];
 var a=list[0];
 var len = list.length;
 var num = 0;
